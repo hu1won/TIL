bfd3139186d247bb0645286ac727e152c0af29c9
Huiwon <71807924+hu1won@users.noreply.github.com>
2021-06-02 13:24:44 +0900

    ts

--- Typescript/test.ts
+++ Typescript/test.ts
@@ -3,4 +3,5 @@ let x; if(foo && foo.bar && foo.bar.baz) { x = foo.bar.baz()}
 let x = foo?.bar?.baz();
 
 function print(doPrint: boolean): any { if (doPrint) { return; } console.log('print'); return { print: print }; } print(false)?.print(false)?.print(true).print(false);
-// console : print // console : print
+// console : print
+// console : print
